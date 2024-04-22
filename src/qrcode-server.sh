8cce898fcfb25d80dacacf143c408778b553263a
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2024-04-22 18:57:08 +0800

    feat: ssh and user login will run difference script

--- src/qrcode-server.sh
+++ src/qrcode-server.sh
@@ -6,25 +6,30 @@ echo qruser:12345678 | chpasswd
 
 echo "---------------------"
 echo "QRCode generate and show image"
-cat > /home/qruser/start << EOF
+cat > /home/qruser/ssh << EOF
 info="http://www.google.com?\$(date +%s)"
 echo -----------------------------
 echo \${info}
 echo -----------------------------
 qrencode -s 1 -m 1 -v 1 -o ~/output \${info}
-catimg ~/output
+catimg -r 1 ~/output
+EOF
+
+cat > /home/qruser/login << EOF
+source ~/ssh
+read -p "Press enter to continue"
 exit 1
 EOF
 
 echo "---------------------"
 echo "Setting non-login user will run startup script"
-echo "source ~/start" | tee -a /home/qruser/.bashrc
+echo "source ~/login" | tee -a /home/qruser/.bashrc
 
 echo "---------------------"
 echo "Setting sshd_config. Make sure ssh login user will run startup script"
 cat > /etc/ssh/sshd_config.d/qrusers.conf << EOF
 Match group qrusers
-    ForceCommand /bin/bash /home/qruser/start
+    ForceCommand /bin/bash /home/qruser/ssh
 EOF
 
 echo "---------------------"
